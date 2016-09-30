6534ef0e40dbde92c27a8e59b3a67ca7bac62775
juanFdS <juanmanuel9794@hotmail.com>
2016-09-30 01:31:17 -0300

    Type Resource erased, public methods are tagged as producers and consumers

--- src/main/scala/org/uqbar/plugins/Core.scala
+++ src/main/scala/org/uqbar/plugins/Core.scala
@@ -1,47 +1,65 @@
 package org.uqbar.plugins
 
 import scala.reflect.runtime.universe._
+import scala.reflect.runtime.universe
 
 object uqbarPlugins {
 
-  val mirror = scala.reflect.runtime.universe.runtimeMirror(getClass.getClassLoader)
-  
-  trait Resource
-  
+  val mirror = universe.runtimeMirror(getClass.getClassLoader)
+
   trait PluggableApplication extends ConsumerProvider {
-    def loadPlugin(plugin: Plugin): Unit =
-      for{producer <- plugin.producers
-          consumer <- consumers} { consumer consume producer.product }
+
+    var plugins: List[Plugin] = Nil
+
+    def producers = plugins flatMap (_.producers)
+
+    def loadPlugin(plugin: Plugin) {
+      plugins :+= plugin
+      for {
+        producer <- plugin.producers
+        consumer <- consumers if (consumer needsType (producer.getType)) && (consumer canBeFulfilledBy producers)
+      } { consumer consume (producer.product) }
+    }
   }
-  
-  trait Plugin extends ConsumerProvider with ProducerProvider 
-  
-  trait ConsumerProvider {
-    val consumers = for{method <- getClass.methods if method.paramLists.exists(_.exists(_.info <:< typeOf[Resource]))}
-    yield Consumer(this, method) 
-  }
-  
+
+  trait Plugin extends ConsumerProvider with ProducerProvider
+
   trait ProducerProvider {
-    val producers = for{method <- getClass.methods if method.returnType <:< typeOf[Resource]}
-    yield Producer(this, method)
+    def producers = getClass.publicMethods.collect { case IsProducer(method) => Producer(this, method) }
   }
-  
+
+  trait ConsumerProvider {
+    def consumers = getClass.publicMethods.collect { case IsConsumer(method) => Consumer(this, method) }
+  }
+
+  object IsProducer {
+    def unapply(method: MethodSymbol) = Option(method) filterNot (_.returnType <:< typeOf[Unit])
+  }
+
+  object IsConsumer {
+    def unapply(method: MethodSymbol) = Option(method) filterNot (_.paramLists.flatten.isEmpty)
+  }
+
   trait ResourceHandler {
     val method: MethodSymbol
     val methodOwner: Any
     def reflectedMethod = mirror.reflect(methodOwner).reflectMethod(method)
   }
-  
-  case class Producer[T <: Resource](methodOwner: Any, method: MethodSymbol) extends ResourceHandler {    
-    def product = reflectedMethod.apply().asInstanceOf[T]
+
+  case class Producer(methodOwner: Any, method: MethodSymbol) extends ResourceHandler {
+    def product = reflectedMethod()
+    def getType = method.returnType
   }
-  
-  case class Consumer[T <: Resource](methodOwner: Any, method: MethodSymbol) extends ResourceHandler {
-    def consume(parameter: T) = reflectedMethod.apply(parameter)
+
+  case class Consumer(methodOwner: Any, method: MethodSymbol) extends ResourceHandler {
+    def consume(parameter: Any) = reflectedMethod(parameter)
+    def getTypes = method.paramLists.flatten.map(_.info)
+    def needsType(aType: Type) = getTypes contains aType
+    def canBeFulfilledBy(producers: Iterable[Producer]) = getTypes forall (tpe => producers exists (tpe <:< _.getType))
   }
-  
+
   implicit class ReflectiveClass(aClass: Class[_]) {
-    def classSymbol = mirror.classSymbol(aClass)
-    def methods = classSymbol.toType.members.filter(_.isMethod).map(_.asMethod)
+    def classSymbol = mirror classSymbol aClass
+    def publicMethods = classSymbol.toType.decls.filter(_.isMethod).filter(_.isPublic).map(_.asMethod)
   }
 }
--- src/test/scala/org/uqbar/plugins/CoreTest.scala
+++ src/test/scala/org/uqbar/plugins/CoreTest.scala
@@ -8,7 +8,7 @@ import uqbarPlugins._
 class CoreTest extends FreeSpec with Matchers with BeforeAndAfterAll {
   "PluggableAppication" - {
     "Load a plugin" in {
-      case class Coffee(name: String) extends Resource
+      case class Coffee(name: String)
       object blackCoffeeProvider extends Plugin {
         def blackCoffee: Coffee = Coffee("Black Coffee")
       }
@@ -20,6 +20,10 @@ class CoreTest extends FreeSpec with Matchers with BeforeAndAfterAll {
           this.coffee = coffee
         }
       }
+      
+      printf(s"Consumers: ${app.consumers.map(_.method.name)}\n")
+      
+      printf(s"Producers: ${blackCoffeeProvider.producers.map(_.method.name)}\n")
       
       app.coffee should be (Coffee("Coffee"))
       
